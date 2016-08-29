38dc06003e053d950e5a4c8de834d677076a7991
dimicuss <dimicuss@gmail.com>
2016-08-29 10:52:54 +0300

    Добавил правки для node 0.10.46

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const foo = require('./vkApi')({
+var foo = require('./vkApi')({
     accessToken: '', 
 });
 //                ^
--- vkApi.js
+++ vkApi.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = function (config) {
-    const request     = require('request');
-    const baseUrl     = 'https://api.vk.com/method/';
-    const version     = '5.53';
-    const accessToken = config.accessToken;
+    var request     = require('request');
+    var baseUrl     = 'https://api.vk.com/method/';
+    var version     = '5.53';
+    var accessToken = config.accessToken;
 
     var withPromise = function ( fn ) {
 	return function (value) {
@@ -16,9 +16,15 @@ module.exports = function (config) {
     var markAsRead = function (message) {
 	var messageId = message.updates[0][1]
 	if( messageId ) {
-	    let reqUrl    = baseUrl + 'messages.markAsRead' + `?access_token=${accessToken}&message_ids=${messageId}&version=` + version;
+	    var reqUrl    = baseUrl + 'messages.markAsRead' +
+		'?access_token=' +
+		accessToken      +
+		'&message_ids='  +
+		messageId        +
+		'&version='      +
+		version;
 	
-	    request.post(reqUrl, (err, res, body) => { 
+	    request.post(reqUrl, function(err, res, body) { 
 		if (!err && res.statusCode == 200) {
 		    console.log(body);
 		    return;
@@ -33,10 +39,16 @@ module.exports = function (config) {
     }
 
     
-    var setLongPollHandler = function (resp) {
-	var reqUrl = `https://${resp.server}?act=a_check&key=${resp.key}&ts=${resp.ts}&wait=25&mode=2`;
-	request.get( reqUrl, (err, res, body) => {
-	    let parsedResp = JSON.parse(body);
+    var setLongPollHandler = function(resp) {
+	var reqUrl = 'https://'+
+	    resp.server +
+	    '?act=a_check&key=' +
+	    resp.key +
+	    '&ts=' +
+	    resp.ts +
+	    '&wait=25&mode=2';
+	request.get( reqUrl, function(err, res, body) {
+	    var parsedResp = JSON.parse(body);
 
 	    if (!err && res.statusCode == 200) {
 		if(parsedResp.updates.length) {
@@ -54,14 +66,17 @@ module.exports = function (config) {
     }
     
     
-    var setLongPollServer = function (params) {
+    var setLongPollServer = function(params) {
 	var url = 'messages.getLongPollServer',
-	    reqUrl = baseUrl + `${url}?access_token=${accessToken}`;
-	   
+	    reqUrl = baseUrl + url +
+	    '?access_token=' +
+	    accessToken;
+	console.log();
 	
-	request.get( reqUrl, (err, res, body) => {
+	request.get( reqUrl, function(err, res, body) {
 	    if (!err && res.statusCode == 200) {
-		let parsedResp = JSON.parse(body);
+		var parsedResp = JSON.parse(body);
+		console.log(parsedResp);
 		setLongPollHandler(parsedResp.response);
 	    }
 	    else {
